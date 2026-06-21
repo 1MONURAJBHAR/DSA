3c65328d29e857ca75f052c4a96bff6f354c4b11
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-06-21 22:10:46 +0530

    update solution

--- StringsAndStringBuilder/Problems/LongestSubstringWithoutRepeatingCharacters.java
+++ StringsAndStringBuilder/Problems/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
@@ -26,6 +28,34 @@ public class LongestSubstringWithoutRepeatingCharacters {
             set.add(s.charAt(right));
             maxLength = Math.max(maxLength, right-left + 1);
         }
+        return maxLength;
+    }
+
+
+    public static int lengthOfLongestSubstring2(String s) {
+
+        int start = 0;
+        int end = 0;
+        int maxLength = 0;
+
+        List<Character> list = new ArrayList<>();
+
+        while (end < s.length()) {
+
+            if (!list.contains(s.charAt(end))) {
+
+                list.add(s.charAt(end));
+                end++;
+
+                maxLength = Math.max(maxLength, list.size());
+
+            } else {
+
+                list.remove(Character.valueOf(s.charAt(start)));
+                start++;
+            }
+        }
+
         return maxLength;
     }
 }
