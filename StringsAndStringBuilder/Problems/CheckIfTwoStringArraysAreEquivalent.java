774787793e61c164ccd92bcb79d58b0856772d79
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-07-04 18:32:14 +0530

    defanging an IP address & Check two array string are equal

---
+++ StringsAndStringBuilder/Problems/CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,25 @@
+public class CheckIfTwoStringArraysAreEquivalent {
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+        for (String str : word1) {
+            s1.append(str);
+        }
+
+        for (String str : word2) {
+            s2.append(str);
+        }
+
+        return s1.toString().equals(s2.toString());
+    }
+
+    public static void main(String[] args) {
+
+        String[] word1 = {"ab", "c"};
+        String[] word2 = {"a", "bc"};
+
+        System.out.println(arrayStringsAreEqual(word1, word2)); // true
+    }
+}
