9124f7447457df2fe22b091b5d17020dda7c06df
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-07-14 13:50:53 +0530

    Count and say

--- StringsAndStringBuilder/Problems/SequentialDigits.java
+++ StringsAndStringBuilder/Problems/SequentialDigits.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class SequentialDigits {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SequentialDigits {
+
+    public static List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int start = 1; start <= 9; start++) {
+            int num = start;
+
+            for (int next = start + 1; next <= 9; next++) {
+                num = num * 10 + next;
+
+                if (num >= low && num <= high) {
+                    ans.add(num);
+                }
+            }
+        }
+
+        Collections.sort(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int low = 100;
+        int high = 10000;
+
+        List<Integer> result = sequentialDigits(low, high);
+
+        System.out.println(result);
+    }
 }
